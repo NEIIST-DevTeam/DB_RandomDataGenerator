cfa3e739c9048e25830a52206eda254d31c6e91e
Nuno Vasconcelos <nunorogerio12@hotmail.com>
2016-12-03 23:21:12 +0000

    First files

---
+++ src/Generator.java
@@ -0,0 +1,10 @@
+
+import Names.Names;
+
+public class Generator {
+
+    public static void main(String[] args)
+    {
+        Names names = new Names();
+    }
+}
---
+++ src/Names/Names.java
@@ -0,0 +1,15 @@
+package Names;
+
+import java.util.ArrayList;
+
+public class Names {
+
+    private ArrayList<String> firstNames = new ArrayList<>();
+    private ArrayList<String> entityNames = new ArrayList<>();
+    private ArrayList<String> organizationNames = new ArrayList<>();
+
+    public Names()
+    {
+        
+    }
+}
