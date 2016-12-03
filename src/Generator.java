475b38573ba6e5cbb345652fa26cf096989cc926
Nuno Vasconcelos <nunorogerio12@hotmail.com>
2016-12-03 23:37:10 +0000

    Now reading from files and storing in ArrayLists

--- src/Generator.java
+++ src/Generator.java
@@ -1,10 +1,15 @@
 
 import Names.Names;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Generator {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         Names names = new Names();
+
+        ArrayList<String> name = names.getFirstNames();
+
     }
 }
--- src/Names/Names.java
+++ src/Names/Names.java
@@ -1,15 +1,42 @@
 package Names;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Names {
 
     private ArrayList<String> firstNames = new ArrayList<>();
+    private ArrayList<String> surnameNames = new ArrayList<>();
     private ArrayList<String> entityNames = new ArrayList<>();
     private ArrayList<String> organizationNames = new ArrayList<>();
 
-    public Names()
+    public Names() throws IOException
     {
-        
+        readFile("src\\Names\\firstname_list.txt", firstNames);
+        readFile("src\\Names\\surname_list.txt", surnameNames);
+    }
+
+    private void readFile(String fileName, ArrayList<String> names) throws IOException
+    {
+
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        try
+        {
+            String line = br.readLine();
+
+            while (line != null)
+            {
+                names.add(line.toString());
+                line = br.readLine();
+            }
+        }
+        finally
+        {
+            br.close();
+        }
+
     }
 }
