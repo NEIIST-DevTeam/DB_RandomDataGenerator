16f84d9436da6b620a7f42acac339a9de5c40936
Nuno Vasconcelos <nunorogerio12@hotmail.com>
2016-12-04 00:27:30 +0000

    Cleaned up some code

--- src/Generator.java
+++ src/Generator.java
@@ -2,14 +2,12 @@
 import Names.Names;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 public class Generator {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         Names names = new Names();
-
-        ArrayList<String> name = names.getFirstNames();
-
     }
 }
